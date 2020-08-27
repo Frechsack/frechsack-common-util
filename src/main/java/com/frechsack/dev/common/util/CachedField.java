34424d56f775c24b59c40121a80cfd051d4ff0e4
Frechsack <61121564+Frechsack@users.noreply.github.com>
2020-08-27 18:02:31 +0200

    Updated CachedField.

--- src/main/java/com/frechsack/dev/common/util/CachedField.java
+++ src/main/java/com/frechsack/dev/common/util/CachedField.java
@@ -16,6 +16,12 @@ public abstract class CachedField<E> implements Result<E>,Disposable
     protected abstract E generate();
 
     /**
+     * Called when the value of this {@link CachedField} is accessed.
+     * @param value The value. Avoid getting the value with {@link CachedField#obtain()}, because that will resolve in an infinite loop.
+     */
+    protected void validate(E value){}
+
+    /**
      * Disposes the current value, if it's set. The next time - the value is required - it will be created again.
      */
     @Override
@@ -29,13 +35,14 @@ public abstract class CachedField<E> implements Result<E>,Disposable
     }
 
     @Override
-    public E obtain()
+    public final E obtain()
     {
         if(!isValue)
         {
             value = generate();
             isValue = true;
         }
+        validate(value);
         return value;
     }
 
