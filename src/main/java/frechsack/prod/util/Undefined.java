78a37926b00ca1a37f5a0cf9f638cb4c90306644
frechsack <patrickwolter.1@gmx.de>
2023-02-10 11:10:56 +0100

    Doc.

--- src/main/java/frechsack/prod/util/Undefined.java
+++ src/main/java/frechsack/prod/util/Undefined.java
@@ -13,6 +13,14 @@ import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
+
+/**
+ * A container object that may contain the state 'undefined', 'null' or 'present'.
+ * In case of 'present' a value can be obtained by {@link #get()}.
+ * If one of the states is 'undefined' or 'null', this object will have the state 'empty' too.
+ * Calling {@link #get()} when this container is empty, will throw an exception.
+ * @param <Type> The container value class-type.
+ */
 public interface Undefined <Type> {
 
     @SuppressWarnings("unchecked")
@@ -113,74 +121,150 @@ public interface Undefined <Type> {
         return (Number<Type>) UndefinedFactory.Undefined.NUMBER;
     }
 
+    /**
+     * Specialised version of {@link Undefined} with additional numeric operations.
+     * @param <Type> The container value class-type.
+     */
     interface Number<Type extends java.lang.Number> extends Undefined<Type> {
 
+        /**
+         * Returns the value as a long.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as a long.
+         */
         long getAsLong();
 
+        /**
+         * Returns the value as a double.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as a double.
+         */
         double getAsDouble();
 
+        /**
+         * Returns the value as an integer.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as an integer.
+         */
         default int getAsInt(){
             return (int) getAsLong();
         }
+
+        /**
+         * Returns the value as a byte.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as a byte.
+         */
         default byte getAsByte(){
             return (byte) getAsInt();
         }
 
+        /**
+         * Returns the value as a short.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as a short.
+         */
         default short getAsShort(){
             return (byte) getAsInt();
         }
 
+        /**
+         * Returns the value as a float.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as a float.
+         */
         default float getAsFloat(){
             return (float) getAsDouble();
         }
 
+        /**
+         * Returns a Stream with one or zero elements.
+         * The Stream will have one element, if this object has the state 'present', otherwise the stream will be empty.
+         * @return Returns the Stream.
+         */
         default IntStream intStream(){
             return isPresent()
                     ? IntStream.of(getAsInt())
                     : IntStream.empty();
         }
 
+        /**
+         * Returns the value in this object, if one is present, otherwise the specified value.
+         * @param value The alternative value.
+         * @return Returns an integer.
+         */
         default int orElse(int value) {
             return isPresent()
                     ? getAsInt()
                     : value;
         }
-
+        /**
+         * Returns a Stream with one or zero elements.
+         * The Stream will have one element, if this object has the state 'present', otherwise the stream will be empty.
+         * @return Returns the Stream.
+         */
         default DoubleStream doubleStream(){
             return isPresent()
                     ? DoubleStream.of(getAsDouble())
                     : DoubleStream.empty();
         }
 
+        /**
+         * Returns the value in this object, if one is present, otherwise the specified value.
+         * @param value The alternative value.
+         * @return Returns a double.
+         */
         default double orElse(double value) {
             return isPresent()
                     ? getAsDouble()
                     : value;
         }
 
+        /**
+         * Returns a Stream with one or zero elements.
+         * The Stream will have one element, if this object has the state 'present', otherwise the stream will be empty.
+         * @return Returns the Stream.
+         */
         default LongStream longStream(){
             return isPresent()
                     ? LongStream.of(getAsLong())
                     : LongStream.empty();
         }
 
+        /**
+         * Returns the value in this object, if one is present, otherwise the specified value.
+         * @param value The alternative value.
+         * @return Returns a long.
+         */
         default long orElse(long value) {
             return isPresent()
                     ? getAsLong()
                     : value;
         }
 
-        default void ifPresent(IntConsumer consumer){
+        /**
+         * Executes the given Consumer, if a value is present.
+         * @param consumer The Consumer to be executed.
+         */
+        default void ifPresent(@NotNull IntConsumer consumer){
             if(isPresent())
                 consumer.accept(getAsInt());
         }
 
-        default void ifPresent(DoubleConsumer consumer){
+        /**
+         * Executes the given Consumer, if a value is present.
+         * @param consumer The Consumer to be executed.
+         */
+        default void ifPresent(@NotNull DoubleConsumer consumer){
             if(isPresent())
                 consumer.accept(getAsDouble());
         }
 
-        default void ifPresent(LongConsumer consumer){
+        /**
+         * Executes the given Consumer, if a value is present.
+         * @param consumer The Consumer to be executed.
+         */
+        default void ifPresent(@NotNull LongConsumer consumer){
             if (isPresent())
                 consumer.accept(getAsLong());
         }
@@ -205,10 +289,23 @@ public interface Undefined <Type> {
 
     }
 
+    /**
+     * Specialised version of {@link Undefined} with booleans.
+     */
     interface Boolean extends Undefined<java.lang.Boolean> {
 
+        /**
+         * Returns the value as a boolean.
+         * @throws java.util.NoSuchElementException, if {@link #isEmpty()} is true.
+         * @return Returns the value as a boolean.
+         */
         boolean getAsBoolean();
 
+        /**
+         * Returns the value in this object, if one is present, otherwise the specified value.
+         * @param value The alternative value.
+         * @return Returns a boolean.
+         */
         default Stream<java.lang.Boolean> orElse(boolean value){
             return isPresent()
                     ? Stream.of(get())
@@ -216,53 +313,102 @@ public interface Undefined <Type> {
         }
     }
 
+    /**
+     * Checks if this object has the state 'undefined'.
+     * @return {@code true} if this object has the state 'undefined', else {@code false}.
+     */
     boolean isUndefined();
 
+    /**
+     * Checks if this object has the state 'null'.
+     * @return {@code true} if this object has the state 'null', else {@code false}.
+     */
     boolean isNull();
 
+    /**
+     * Returns the value of this object.
+     * @throws java.util.NoSuchElementException, if this Undefined has not the state 'present'.
+     * @return Returns the value or throws an Exception.
+     */
     Type get();
 
+    /**
+     * Checks if this object has the state 'present'.
+     * @return {@code true} if this object has the state 'present', else {@code false}.
+     */
     default boolean isPresent(){
         return !isNull() && !isUndefined();
     }
 
+    /**
+     * Checks if this object has the state 'empty'.
+     * @return {@code true} if this object has the state 'present', else {@code false}.
+     */
     default boolean isEmpty(){
         return isNull() || isUndefined();
     }
 
+    /**
+     * Returns a {@link Stream} with one or zero elements.
+     * The Stream will have one element, if this object has the state 'present', otherwise the stream will be empty.
+     * @return Returns a Stream.
+     */
     default Stream<Type> stream(){
         return isPresent()
                 ? Stream.of(get())
                 : Stream.empty();
     }
 
+    /**
+     * Returns a {@link Stream} with one or zero elements.
+     * This version includes {@code null} as an element.
+     * @return Returns a Stream.
+     */
+    default Stream<Type> streamIncludeNull(){
+        return isPresent()
+                ? Stream.of(get())
+                : isNull()
+                    ? Stream.of((Type) null)
+                    : Stream.empty();
+    }
+
+    /**
+     * Returns the value in this object, if one is present, otherwise the specified value.
+     * @param value The alternative value.
+     * @return Returns a long.
+     */
     default Type orElse(Type value) {
         return isPresent()
                 ? get()
                 : value;
     }
 
-    default Type orElseThrow(Supplier<? extends RuntimeException> exception){
+    /**
+     * Acts like {@link #get()}, but will throw a customized Exception, if this object is 'empty'.
+     * @param exception The Exception that should be thrown, if this object is 'empty'.
+     * @return Returns the value or throws an Exception.
+     */
+    default Type orElseThrow(@NotNull Supplier<? extends RuntimeException> exception){
         if(isEmpty()) throw exception.get();
         return get();
     }
 
-    default void ifPresent(Consumer<? super Type> consumer){
+    default void ifPresent(@NotNull Consumer<? super Type> consumer){
         if(isPresent())
             consumer.accept(get());
     }
 
-    default void ifEmpty(Runnable runnable){
+    default void ifEmpty(@NotNull Runnable runnable){
         if(isEmpty())
             runnable.run();
     }
 
-    default void ifNull(Runnable runnable){
+    default void ifNull(@NotNull Runnable runnable){
         if(isNull())
             runnable.run();
     }
 
-    default void ifUndefined(Runnable runnable){
+    default void ifUndefined(@NotNull Runnable runnable){
         if(isUndefined())
             runnable.run();
     }
@@ -271,6 +417,10 @@ public interface Undefined <Type> {
         return isPresent()
                 ? Optional.of(get())
                 : Optional.empty();
+    }
+
+    default <Target> Target compute(@NotNull Function<Undefined<Type>, Target> function){
+        return function.apply(this);
     }
 
 }
