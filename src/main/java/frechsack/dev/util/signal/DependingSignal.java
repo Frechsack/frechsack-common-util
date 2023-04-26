2ee0e36208a7801a1c0029d94b46a25933f2473f
Frechsack <patrick.wolter@pwolter.de>
2023-04-26 22:33:32 +0200

    Store references to subscriptions for clean up.

--- src/main/java/frechsack/dev/util/signal/DependingSignal.java
+++ src/main/java/frechsack/dev/util/signal/DependingSignal.java
@@ -3,24 +3,54 @@ package frechsack.dev.util.signal;
 import frechsack.prod.util.concurrent.flow.AutoUnsubscribeSubscriber;
 import frechsack.prod.util.concurrent.flow.CompactSubscriber;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
 
+import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
-import java.util.stream.Stream;
+import java.util.Collection;
+import java.util.concurrent.Flow;
 
 abstract sealed class DependingSignal<Type> extends ObservableSignal<Type> implements Signal<Type> permits DependingBooleanSignal, DependingDoubleSignal, DependingIntSignal, DependingLongSignal, DependingObjectSignal {
 
     boolean isValid = false;
 
-    public DependingSignal(@NotNull Stream<Signal<?>> parents){
-        parents.forEach(it -> it.subscribeOnInvalidate(
-                new AutoUnsubscribeSubscriber<>(new WeakReference<>(this),
-                        new CompactSubscriber<>(Long.MAX_VALUE) {
-                            @Override
-                            public void onNext(Signal<?> item) {
-                                onParentInvalidated();
-                            }
+    private final WeakReference<Flow.Subscription>[] parentSubscriptionReferences;
+
+    public DependingSignal(@NotNull @UnmodifiableView Collection<Signal<?>> parents){
+        //noinspection unchecked
+        this.parentSubscriptionReferences = new WeakReference[parents.size()];
+        final var parentInvalidationSubscriber = new AutoUnsubscribeSubscriber<>(new WeakReference<>(this), new CompactSubscriber<>(Long.MAX_VALUE) {
+            @Override
+            public void onNext(Object item) {
+                onParentInvalidated();
+            }
+
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                super.onSubscribe(subscription);
+                synchronized (parentSubscriptionReferences) {
+                    for (int i = 0; i < parentSubscriptionReferences.length; i++) {
+                        if (parentSubscriptionReferences[i] == null || parentSubscriptionReferences[i].refersTo(null)) {
+                            parentSubscriptionReferences[i] = new WeakReference<>(subscription);
+                            break;
                         }
-                )));
+                    }
+                }
+            }
+        });
+        parents.forEach(it -> it.subscribeOnInvalidate(parentInvalidationSubscriber));
+
+    }
+
+    @Override
+    public synchronized void close() {
+        for (Reference<Flow.Subscription> parentSubscriptionReference : parentSubscriptionReferences) {
+            if (parentSubscriptionReference != null) {
+                Flow.Subscription subscription = parentSubscriptionReference.get();
+                if (subscription != null)
+                    subscription.cancel();
+            }
+        }
     }
 
     private synchronized void onParentInvalidated(){
