2789dceb1392e7e35092fd9db3600c6598ad5106
frechsack <patrick.wolter@pwolter.de>
2023-06-08 19:05:46 +0200

    Test with special executor.

--- src/test/java/frechsack/dev/util/signal/SignalTest.java
+++ src/test/java/frechsack/dev/util/signal/SignalTest.java
@@ -3,15 +3,18 @@ package frechsack.dev.util.signal;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.Executors;
+
 public class SignalTest {
 
 
     @Test
     public void expressionTest() throws InterruptedException {
-        var width = Signal.ofInt(1920);
-        var height = Signal.ofInt(1080);
-        var pixelCount = Signal.pipeOfInt(() -> width.getAsInt() * height.getAsInt(), width, height).build();
-        var pixelCountI = Signal.pipeOfInt(() -> pixelCount.getAsInt() / 2, pixelCount).build();
+        var executor = Executors.newFixedThreadPool(1);
+        var width = Signal.ofInt(1920, executor);
+        var height = Signal.ofInt(1080, executor);
+        var pixelCount = Signal.pipeOfInt(() -> width.getAsInt() * height.getAsInt(), executor, width, height).build();
+        var pixelCountI = Signal.pipeOfInt(() -> pixelCount.getAsInt() / 2, executor, pixelCount).build();
 
         Assert.assertEquals(1080, height.getAsInt());
         Assert.assertEquals(1920, width.getAsInt());
@@ -22,6 +25,6 @@ public class SignalTest {
 
         Thread.sleep(200);
         Assert.assertEquals(720 * 1080 / 2, pixelCountI.getAsInt());
-
+        executor.shutdown();
     }
 }
